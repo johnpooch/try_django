5f4c955dfc70eab7c1149ca990251ab3e611f579
John McDowell <johnmcdowell0801@gmail.com>
2018-06-11 15:34:05 +0000

    django working

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Book
+from .models import Author
 
 # Create your views here.
 def get_index(request):
-    return render(request, "home/index.html")
+    
+    books = Book.objects.all()
+    authors = Author.objects.all()
+    
+    return render(request, "home/index.html", {'books': books, 'authors': authors})
